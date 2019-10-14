fb8603344185c6a1ae6462fac7655a1ad811e889
abrarShariar <shariarabrar@gmail.com>
2019-10-14 14:10:53 +0600

    label data

--- run_analysis.R
+++ run_analysis.R
@@ -14,6 +14,7 @@ subject_test <- read.table("dataset/uci_dataset/test/subject_test.txt")
 x_test <- read.table("dataset/uci_dataset/test/X_test.txt")
 y_test <- read.table("dataset/uci_dataset/test/y_test.txt")
 
+# ---- start of Q:1 ----
 # col bind y_test and subject_test
 test_m <- cbind(subject_test, y_test)
 colnames(test_m) <- c("subject","label")
@@ -26,10 +27,15 @@ merge_data <- rbind(train_m, test_m)
 dir.create(file.path("result-dataset"))
 write.csv(merge_data, file="result-dataset/1-merge-data.csv")
 
-
 # ----- start of Q:2 -----
 # filter feature indexs from features.txt
 d <- read.table("dataset/uci_dataset/features.txt")
 col_indices <- c(paste("V",d[grep("std", d$V2),"V1"], sep=""), paste("V",d[grep("[Mm]ean", d$V2),"V1"], sep=""))
 extracted_data <- merge_data[,c("subject", "label",col_indices)]
 write.csv(extracted_data, file="result-dataset/2-extract-data.csv")
+
+# ---- start of Q:3 ----
+labels <- read.table("dataset/uci_dataset/activity_labels.txt")
+colnames(labels) <- c("no", "text")
+label_data <- merge(labels, merge_data, by.x="no", by.y="label")
+write.csv(label_data, file="result-dataset/3-label-data.csv")
